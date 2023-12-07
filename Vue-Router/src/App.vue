b8f0d2128fe5fe6885320213e0649c6dd24b676f
zhengzhang <zhengzhang@ruijie.com.cn>
2023-12-07 09:19:14 +0800

    Vue-Router

--- Vue-Router/src/App.vue
+++ Vue-Router/src/App.vue
@@ -1,17 +1,8 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
   <div>
-    <!-- <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a> -->
-
-    <h1>当前路由信息：{{ $route }}</h1>
     <!--使用 router-link 组件进行导航 -->
     <!--通过传递 `to` 来指定链接 -->
     <ol>
@@ -21,22 +12,12 @@ import HelloWorld from './components/HelloWorld.vue'
     <hr />
     <!-- 路由出口 -->
     <!-- 路由匹配到的组件将渲染在这里 -->
-    <router-view/>    
+    <router-view/>  
+    <h1>当前路由信息：{{ $route }}</h1>  
   </div>
 
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
